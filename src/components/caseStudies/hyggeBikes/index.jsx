60a9157ee7d78e8a71ef9f622b5277ed510c2d89
Expobird <muzammil.expobird@gmail.com>
2024-08-15 13:01:12 +0500

    changes

--- src/components/caseStudies/hyggeBikes/index.jsx
+++ src/components/caseStudies/hyggeBikes/index.jsx
@@ -1,26 +1,24 @@
 import BreadcrumbEight from "@/common/breadcrumbs/breadcrumb-8";
 import FooterFour from "@/layout/footers/footer-4";
 import React from "react";
-import Brand from "../../about/brand";
-// import ServiceArea from "../../service/service-area";
-import ContactArea2 from "../../homes/home/contact-area2";
-import ServiceArea2 from "../../homes/home-2/service-area";
-import ContactArea from "../../homes/home/contact-area";
+import ContactArea from "./contact-area";
 import BlogArea from "../../homes/home/blog-area";
 import FaqArea from "../../homes/home/faq-area";
-import TestimonialArea from "../../homes/home/testimonial-area-1";
+import TestimonialArea from "../../homes/home/testimonial-area";
 import ProjectArea from "../../homes/home/project-area";
 import PaymentMethodArea from "../../homes/home/payment-method-area";
 import HeaderSix from "@/layout/headers/header-6";
 import ThumbArea from "@/components/project-details/thumb-area";
 import RankArea from "@/components/homes/home/rank-area";
-import AboutArea from "../../../common/about-area";
+import AboutArea from "./about-area";
+import AboutArea1 from "./about-area1";
 import PaymentArea from "@/components/homes/home-2/payment-area";
 import ProjectDetailsArea from "@/components/project-details/project-details-area";
 import CounterArea from "@/components/homes/home-3/counter-area";
 import OpenAccountArea from "@/components/homes/home-2/open-account-area";
 import ServiceArea from "@/components/homes/home-2/service-area";
 import SalesArea from "../../../common/sales-area";
+import FunFactArea from "./fun-fact-area";
 
 const HyggeBikes = () => {
   return (
@@ -33,7 +31,7 @@ const HyggeBikes = () => {
             <ThumbArea />
             <ProjectDetailsArea />
             <RankArea/>
-            <AboutArea/>
+            <AboutArea1/>
             <PaymentArea />
             <CounterArea/>
             <OpenAccountArea />
@@ -43,6 +41,7 @@ const HyggeBikes = () => {
             <ProjectArea />
             <TestimonialArea />
             <AboutArea />
+            <FunFactArea />
             <FaqArea />
             <BlogArea />
             <ContactArea />
